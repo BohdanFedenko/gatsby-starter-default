4f395f2880c08eb1d293048c5db6a30c02477c7f
FedenkoB <bohfed@qamadness.com>
2021-08-09 08:15:50 +0300

    Delete tests folder

--- src/test/utils/BaseComponentSteps.js
+++
@@ -1,9 +0,0 @@
-import Logger from "./Logger";
-import AllureLogger from "./AllureLogger";
-
-export default class BaseComponentSteps {
-
-    step = new AllureLogger().step();
-
-    log = new Logger().additionalInfo();
-}
